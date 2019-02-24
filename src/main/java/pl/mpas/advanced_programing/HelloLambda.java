030969c7dc79df9511bc867909caa54dc17c6a3b
Ihor Chubatenko <doubleredbull@gmail.com>
2019-02-24 10:15:59 +0100

    [add] - work in progress

---
+++ src/main/java/pl/mpas/advanced_programing/HelloLambda.java
@@ -0,0 +1,9 @@
+package pl.mpas.advanced_programing;
+
+public class HelloLambda {
+
+    public static void main(String[] args) {
+        String hw = "Hello world!";
+        System.out.println(hw);
+    }
+}
